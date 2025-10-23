2f0a30a79f5c6389694d7c546a675b5e7049513e
thegoat748256 <divyagopinath@gmail.com>
2025-10-23 19:02:56 +0530

    PY:Added the turtle to after class project

---
+++ AFTER CLASS PROJECTS/Turtles after class project/turtles.py
@@ -0,0 +1,14 @@
+import turtle
+turtle.Screen().bgcolor("yellow")
+turtle.Screen().setup(400,300)
+turtle.title("Turtle Screen")
+
+pen = turtle.Turtle()
+pen.forward(100)
+pen.left(90)
+pen.forward(50)
+pen.left(90)
+pen.forward(100)
+pen.left(90)
+pen.forward(50)
+turtle.done()
