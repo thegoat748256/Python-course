213a7bb8013b8f6a4a303dd456eef9596fb010af
thegoat748256 <divyagopinath@gmail.com>
2025-10-30 18:54:45 +0530

    PY

--- AFTER CLASS PROJECTS/Condenstaional loops/Condensaton.py
+++ AFTER CLASS PROJECTS/Condenstaional loops/Condensaton.py
@@ -2,6 +2,6 @@ number = int(input("Please enter a number"))
 if number>0:
     print("This is a natural number")
 elif number<0:
-    print("This is a unnatural number")
+    print("This is not a natural number")
 else:
     print("This is a neutral number")
