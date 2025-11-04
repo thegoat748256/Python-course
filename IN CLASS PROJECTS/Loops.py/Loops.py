dc8660b90a8a25a564fcb8502646d0394e8ca7c1
thegoat748256 <divyagopinath@gmail.com>
2025-11-04 18:54:52 +0530

    PY:Added the loops 4/11/2025

---
+++ IN CLASS PROJECTS/Loops.py/Loops.py
@@ -0,0 +1,6 @@
+number = 50
+
+for i in range(1,13):
+    
+    result = number*i
+    print(number,"X",i,"=",result)
---
+++ IN CLASS PROJECTS/Loops.py/While loops.py
@@ -0,0 +1,7 @@
+start = 1
+sum = 0
+while(start<=10):
+    sum = sum + start
+    start = start + 1
+
+print(sum)
