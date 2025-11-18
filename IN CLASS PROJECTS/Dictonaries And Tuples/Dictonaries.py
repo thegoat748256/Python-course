080794cd219aae64c0ed89f5cdfd9ab342834845
thegoat748256 <divyagopinath@gmail.com>
2025-11-18 18:51:52 +0530

    PY:Added the dictonary and tuple 18/11/2025

---
+++ IN CLASS PROJECTS/Dictonaries And Tuples/Dictonaries.py
@@ -0,0 +1,23 @@
+my_dictonary = {}
+
+my_dictonary = {"name": "Joshua", "age": 10}
+
+my_dictonary["age"] = 12
+
+print(my_dictonary)
+
+my_dictonary["city"] = "Chennai"
+
+print(my_dictonary)
+
+my_dictonary.pop("age")
+
+print(my_dictonary)
+
+home = my_dictonary.get("city")
+
+print(home)
+
+my_dictonary.clear()
+
+print(my_dictonary)
---
+++ IN CLASS PROJECTS/Dictonaries And Tuples/Tuples.py
@@ -0,0 +1,9 @@
+my_tuple = (1,2,3,4,5)
+
+my_tuple = ("Joshua","Michael","Abhishek",6.7,"Out of",10)
+
+print(my_tuple[4])
+
+my_tuple = (("J","o","s","h","u","a"),(10,11,12,13,14,15,16))
+
+print(my_tuple[1][2])
