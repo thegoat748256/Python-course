1116d90ed0a7c730eefc837d912d35ee7642c4cc
thegoat748256 <divyagopinath@gmail.com>
2025-12-01 20:33:17 +0530

    PY:Basically finnished a major project using python 1/12/2025

--- AFTER CLASS PROJECTS/Slam Book/Slambook.py
+++ AFTER CLASS PROJECTS/Slam Book/Slambook.py
@@ -1,7 +1,7 @@
 #sys module importing
 import sys
 
-def initial_Slambook():
+def isb():
     no_friends = int(input("Enter the number of friends. "))
     no_details = 5
 
@@ -23,4 +23,101 @@ def initial_Slambook():
             if j == 2:
                 tempo.append(str(input("Enter friend's nickname. ")))
                 if tempo[j] == "":
-                    
+                    tempo[j] = None
+
+            if j == 3:
+                tempo.append(str(input("Enter friend's message about you. ")))
+                if tempo[j] == "":
+                    tempo[j] = None
+
+            if j == 4:
+                tempo.append(str(input("Enter friend's date of birth (DOB). ")))
+                if tempo[j] == "":
+                    tempo[j] = None
+        slambook.append(tempo)
+    print(slambook)
+    return slambook
+
+def options():
+    print("***************************************************************************")
+    print("You can now perform these following actions: ")
+    print("1. Add a friend. ")
+    print("2. Delete a friend. ")
+    print("3. Delete ALL friends. ")
+    print("4. Search for an existing friend. ")
+    print("5. Show all friends. ")
+    print("6. Exit/close this slambook. ")
+    print("***************************************************************************")
+    choice = int(input("Enter choice number 1-6: "))
+    return choice
+
+def Add_friend(sb):
+    Dip = []
+    for i in range (len(sb)):
+        if i == 0:
+            Dip.append (str(input("Enter friend's name. ")))
+
+        if i == 1:
+            Dip.append (int(input("Enter friend's contact number. ")))
+
+        if i == 2:
+            Dip.append (str(input("Enter friend's nickname. ")))
+
+        if i == 3:
+            Dip.append (str(input("Enter friend's message about you. ")))
+
+        if i == 4:
+            Dip.append (int(input("Enter friend's date of birth (DOB) dd/mm/yyyy. ")))
+    sb.append(Dip)
+    return sb
+
+def delete_friend(sb):
+        remove = str(input("Enter the name of the friend you want to remove. "))
+        tempoo = 0
+        for i in range (len(sb)):
+            if remove == sb[i][0]:
+                tempoo = tempoo + 1
+                print(sb.pop(i))
+                print("This friend has been removed. ")
+                return sb
+        if tempoo == 0:
+            print("This friend does not exist and we are not able to remove this friend. Try again or exit. ")
+            return sb
+
+def delete_all(sb):
+    print("All friends are deleted. ")
+    return sb.clear()
+
+
+#main function
+print("**********************************************************")
+print("Welcome to the slambook. ")
+print("Proceed further to explore. ")
+print("**********************************************************")
+
+
+
+#Choices (1,2,3,4,5)
+ch = 1
+sb = initial_slambook()
+while ch in (1,2,3,4,5):
+    ch = menu()
+    if ch == 1:
+         sb = Add_friend(sb)
+
+    elif ch == 2:
+         sb = delete_friend(sb)
+
+    elif ch == 3:
+         sb = delete_all(sb)
+
+    elif ch == 4:
+         d = search_existing(sb)
+         if d == -1:
+              print("No such friend exists. Try again. ")
+
+    elif ch == 5:
+         display_all(sb)
+
+    else:
+         thanks(sb)
--- IN CLASS PROJECTS/Phonebook 1/Phonebook.py
+++ IN CLASS PROJECTS/Phonebook 1/Phonebook.py
@@ -38,6 +38,7 @@ def initial_phonebook():
      print(phone_book)
      return phone_book
 
+#defining functions
 def menu():
      print("_____________________________________________________________")
      print("You can now perform all of these actions:")
@@ -49,12 +50,13 @@ def menu():
      print("6. exit/close the phonebook")
      print("_____________________________________________________________")
 
-     choice = int(input("Enter your choice number (1-5). "))
+     choice = int(input("Enter your choice number (1-6). "))
      return choice
 
 def add_contact(pb):
      dip = []
-     for i in range (pb([0])):
+     no_Details = 5
+     for i in range (no_Details):
           if i == 0:
                dip.append (str(input("Enter contact name. ")))
 
@@ -65,7 +67,7 @@ def add_contact(pb):
                dip.append (str(input("Enter email address. ")))
 
           if i == 3:
-               dip.append (int(input("Enter date of birth (DOB) dd/mm/yy. ")))
+               dip.append (str(input("Enter date of birth (DOB) dd/mm/yyyy. ")))
 
           if i == 4:
                dip.append (str(input("Enter category (family/friends/work/other). ")))                    
@@ -84,32 +86,91 @@ def remove_existing(pb):
                return pb
 
      if temp == 0:
-          print("This contct is not found.")
+          print("This contact is not found.")
           return pb
      
 def delete_all(pb):
-     pb.clear()
-     print("All contacts cleared from your phonebook.")
-          
+     confirm =  str(input("Do you want to delete all contacts? You cannot retrieve your contacts back. Type N or Y. "))
+     if confirm in("Y","y"):
+          print("You have successfuly deleted all contacts. ")
+          return pb.clear()
+     elif confirm in("N","n"):
+          print("No contacts deleted. ")
+          return pb
+     else:
+          print("This is not valid. Try again. ")
+
+
+def search_existing(pb):
+     temp = []
+     check = -1
+     choice = int(input("Enter search criteria 1. Name. 2. Number 3. Email-id 4. DOB 5. Category(Family/Friends/Work/Others). Please enter: "))
+
+     if choice == 1:
+          query = str(input("Enter contact name. "))
+          for i in range(len(pb)):
+               if query == pb[i][0]:
+                    check = 1
+                    temp.append(pb[i])
+
+     elif choice == 2:
+          query = int(input("Enter contact number. "))
+          for i in range(len(pb)):
+               if query == pb[i][1]:
+                    check = 1
+                    temp.append(pb[i])
+
+     elif choice == 3:
+          query = str(input("Enter contact email. "))
+          for i in range(len(pb)):
+               if query == pb[i][2]:
+                    check = 1
+                    temp.append(pb[i])
+
+     elif choice == 4:
+          query = str(input("Enter contact date of birth (DOB) dd/mm/yyyy. "))
+          for i in range(len(pb)):
+               if query == pb[i][3]:
+                    check = 1
+                    temp.append(pb[i])
+
+     elif choice == 5:
+          query = str(input("Enter contact category. "))
+          for i in range(len(pb)):
+               if query == pb[i][4]:
+                    check = 1
+                    temp.append(pb[i])
+
+     else:
+          print("Invalid search criteria. Try again. ")
+          return -1
+
+     if check == -1:
+          return -1
+
+     else:
+          display_all(temp)
+          return check
+
+
+def display_all(pb): 
+	if not pb: 
+		print("List is empty: []") 
+	else: 
+		for i in range(len(pb)): 
+			print(pb[i])
+
+
+def thanks():
+     print("___________________________________________________________________")
+     print("Thank you for trying out this directory system. ")
+     print("Please visit again! ")
+     print("___________________________________________________________________")
+     sys.exit("Have a great day ahead!")
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#main part
  #Welcome msg
 print("____________________________________________________________________________")
 print("Welcome to the phonebook.")
@@ -139,4 +200,4 @@ while ch in (1,2,3,4,5):
          display_all(pb)
 
     else:
-         thanks(pb)
+         thanks()
