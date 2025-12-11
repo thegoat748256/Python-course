3861a75bd795c7a22a5783f2659434fdb88902b0
thegoat748256 <divyagopinath@gmail.com>
2025-12-11 19:12:27 +0530

    PY:Fixed typos 11/12/2025

--- IN CLASS PROJECTS/Capstone Project(1)/Capstone Project.py
+++ IN CLASS PROJECTS/Capstone Project(1)/Capstone Project.py
@@ -10,6 +10,7 @@ def Start_Game():
     RNG = int(random.randint(1,10))
     print("Hi! Welcome to the guessing game!")
     Player_Name = str(input("Before starting this game, Please enter your name. "))
+    print("Hello ",Player_Name)
     Choice = input("Do you want to play the game? Type yes or no. ")
     Attemps_Made = 0
     score_displaying()
@@ -18,17 +19,7 @@ def Start_Game():
             Guess = int(input("Please enter a number from 1 - 10. "))
             if Guess <1 or Guess >10:
                 raise ValueError("The number is out of range, Please try again. ")
-            if Guess == RNG:
-                print("Great job! you guessed my number! ")
-                Attemps_Made = Attemps_Made + 1
-                no_attemps.append(Attemps_Made)
-                print("This is how many attemps you took to guess. Great job! :",Attemps_Made)
-                Play_Again = input("Do you want to play again? Type yes or no. ")
-                Attemps_Made = 0
-                if Play_Again.upper() == "NO":
-                    print("Ok, visit again and have a great day ahead!")
-                    break
-            
+           
             if Guess <RNG:
                 print("The number you entered is lower than the number generated.")
                 Attemps_Made = Attemps_Made + 1
@@ -36,6 +27,19 @@ def Start_Game():
             if Guess >RNG:
                 print("The number you entered is higher than the number generated. ")
                 Attemps_Made = Attemps_Made + 1
+
+            if Guess == RNG:
+                print("Great job! you guessed my number! ")
+                Attemps_Made = Attemps_Made + 1
+                no_attemps.append(Attemps_Made)
+                print("This is how many attempts you took to guess. Great job! :",Attemps_Made)
+                Play_Again = input("Do you want to play again? Type yes or no. ")
+                Attemps_Made = 0
+                score_displaying()
+                RNG = int(random.randint(1,10))
+                if Play_Again.upper() == "NO":
+                    print("Ok, visit again and have a great day ahead!")
+                    break
 
         except ValueError as ERROR:
            print("This is not a valid input. Please try again. ")
